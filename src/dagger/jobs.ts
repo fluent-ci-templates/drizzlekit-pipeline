66a7b91df0d57a52a9d22de178d88aa61f90627e
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-09-04 07:14:04 +0000

    fix postgres env variables

--- src/dagger/jobs.ts
+++ src/dagger/jobs.ts
@@ -11,30 +11,17 @@ const DATABASE_URL = Deno.env.get("DATABASE_URL");
 const exclude = [".git", "node_modules", ".fluentci"];
 
 export const check = async (client: Client, src = ".") => {
-  if (!DATABASE_URL) {
-    throw new Error("DATABASE_URL is not set");
-  }
-
-  const postgres = client
-    .container()
-    .from("postgres:15-alpine")
-    .withEnvVariable("POSTGRES_PASSWORD", "pass")
-    .withEnvVariable("POSTGRES_DATABASE", "example")
-    .withExposedPort(5432);
-
   const context = client.host().directory(src);
   const ctr = client
     .pipeline(Job.check)
     .container()
     .from("ghcr.io/fluent-ci-templates/bun:latest")
-    .withServiceBinding("postgres", postgres)
     .withMountedCache(
       "/app/node_modules",
       client.cacheVolume("drizzle_node_modules")
     )
     .withDirectory("/app", context, { exclude })
     .withWorkdir("/app")
-    .withEnvVariable("DATABASE_URL", DATABASE_URL)
     .withExec(["sh", "-c", 'eval "$(devbox global shellenv)" && bun install'])
     .withExec([
       "sh",
@@ -56,7 +43,7 @@ export const push = async (client: Client, src = ".") => {
     .container()
     .from("postgres:15-alpine")
     .withEnvVariable("POSTGRES_PASSWORD", "pass")
-    .withEnvVariable("POSTGRES_DATABASE", "example")
+    .withEnvVariable("POSTGRES_DB", "example")
     .withExposedPort(5432);
 
   const context = client.host().directory(src);
@@ -65,7 +52,6 @@ export const push = async (client: Client, src = ".") => {
     .container()
     .from("ghcr.io/fluent-ci-templates/bun:latest")
     .withServiceBinding("postgres", postgres)
-    .withExec(["apk", "update"])
     .withMountedCache(
       "/app/node_modules",
       client.cacheVolume("drizzle_node_modules")
